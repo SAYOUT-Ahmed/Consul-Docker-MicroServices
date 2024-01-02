f6e5ca7a4ab097e42ac076bdd4273adc6cc74aaa
Ahmed <ahmed.sayout18@gmail.com>
2024-01-02 01:57:55 +0100

    consul microservice

--- car/src/main/java/com/example/car/CarApplication.java
+++ car/src/main/java/com/example/car/CarApplication.java
@@ -2,6 +2,7 @@ package com.example.car;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -2,13 +2,35 @@ package com.example.client;
 
 import com.example.client.entities.Client;
 import com.example.client.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientApplication {
+
+    /*@GetMapping("/accessAPI")
+    public String invokeHelloWorld(){
+        URI Service = discoveryClient.getInstances("SERVER").stream().map(serviceInstance -> serviceInstance.getUri()).findFirst()
+                .map((s-> s.resolve("/hello"))).get();
+        return template().getForObject("", String.class);
+    }*/
+
+   /* @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;*/
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
@@ -22,4 +44,9 @@ public class ClientApplication {
             clientRepository.save(new Client(Long.parseLong("3"), "Samir RAMI", Float.parseFloat("22")));
         };
     }
+
+    /*@Bean
+    public RestTemplate template(){
+        return new RestTemplate();
+    }*/
 }
